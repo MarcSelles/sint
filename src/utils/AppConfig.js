9d5c94c05b00f68e59dd25e883d8fb033bddc31d
Marc Selles <marc@bikkelhart.com>
2022-11-26 21:11:02 +0100

    Remove unused code

--- src/utils/AppConfig.js
+++ src/utils/AppConfig.js
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  site_name: 'Bikkelhart Starter',
+  site_name: 'Elise Wijema',
   title: 'Nextjs Starter',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'nl'
